9ca9d824f4524a218bb50c880e643e289108d602
Stephan Granzow <s.granzow@planhero.de>
2023-04-04 13:17:24 +0200

    new design der Note

--- mobile/lib/Pages/NoteList.dart
+++ mobile/lib/Pages/NoteList.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile/Pages/createNotePage.dart';
-import 'package:mobile/Pages/updateNotePage.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile/Pages/updateNotePage.dart';
 
 class NoteList extends StatefulWidget {
   const NoteList({super.key});
@@ -72,12 +72,12 @@ class _NoteListState extends State<NoteList> {
       appBar: AppBar(
         title: const Text('Notes'),
       ),
-      body: ListView.builder(
+      body: GridView.builder(
         itemCount: notes.length,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          print(notes);
           final note = notes[index];
-
           return GestureDetector(
             onLongPress: () {
               Navigator.push(
@@ -95,66 +95,179 @@ class _NoteListState extends State<NoteList> {
               // Navigator.of(context).pop();
             },
             child: Card(
-                color: const Color.fromARGB(255, 215, 195, 137),
-                child: Column(
-                  children: [
-                    Text(
-                      notes[index]['title'],
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                    Text(
-                      notes[index]['text'],
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                    Text(
-                      DateFormat("dd.MM.yyyy HH:mm")
-                          .format(DateTime.parse(notes[index]['createdAt'])),
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Delete note'),
-                              content: const Text(
-                                  'Are you sure you want to delete this note?'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Cancel'),
-                                  onPressed: () => Navigator.of(context).pop(),
-                                ),
-                                TextButton(
-                                  child: const Text('Delete'),
-                                  onPressed: () {
-                                    setState(() {
-                                      deleteNote(notes[index]['_id']);
-                                      Navigator.of(context).pop();
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const NoteList()),
-                                      );
-                                    });
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.red,
+              color: const Color.fromARGB(255, 215, 195, 137),
+              child: GridTile(
+                header: Text(
+                  notes[index]['title'],
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                footer: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        DateFormat("dd.MM.yyyy HH:mm")
+                            .format(DateTime.parse(notes[index]['createdAt'])),
+                        style: TextStyle(
+                            color: Colors.grey.shade800, fontSize: 12),
                       ),
+                      IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Delete note'),
+                                content: const Text(
+                                    'Are you sure you want to delete this note?'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Cancel'),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(),
+                                  ),
+                                  TextButton(
+                                    child: const Text('Delete'),
+                                    onPressed: () {
+                                      setState(() {
+                                        deleteNote(notes[index]['_id']);
+                                        Navigator.of(context).pop();
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const NoteList()),
+                                        );
+                                      });
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.grey.shade900,
+                          size: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      notes[index]['text'],
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15),
                     ),
-                  ],
-                )),
+                  ),
+                ),
+              ),
+            ),
           );
         },
       ),
+
+      // ListView.builder(
+      //   itemCount: notes.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     print(notes);
+      //     final note = notes[index];
+
+      //     return GestureDetector(
+      //       onLongPress: () {
+      //         Navigator.push(
+      //             context,
+      //             MaterialPageRoute(
+      //                 builder: (context) => UpdateNotePage(
+      //                     id: notes[index]['_id'],
+      //                     title: notes[index]['title'],
+      //                     text: notes[index]['text'])));
+      //         // updateNotes(
+      //         //     notes[index]['_id'],
+      //         //     notes[index]['title'],
+      //         //     notes[index]['text'],
+      //         //     notes[index]['updatedAt']);
+      //         // Navigator.of(context).pop();
+      //       },
+      //       child: SizedBox(
+      //         height: 150,
+      //         child: Card(
+      //             color: const Color.fromARGB(255, 215, 195, 137),
+      //             child: Column(
+      //               children: [
+      //                 Text(
+      //                   notes[index]['title'],
+      //                   style: const TextStyle(
+      //                       color: Colors.black,
+      //                       fontWeight: FontWeight.bold,
+      //                       fontSize: 20),
+      //                 ),
+      //                 Text(
+      //                   notes[index]['text'],
+      //                   style: const TextStyle(color: Colors.black),
+      //                 ),
+      //                 Text(
+      //                   DateFormat("dd.MM.yyyy HH:mm")
+      //                       .format(DateTime.parse(notes[index]['createdAt'])),
+      //                   style: const TextStyle(color: Colors.black),
+      //                 ),
+      //                 IconButton(
+      //                   onPressed: () {
+      //                     showDialog(
+      //                       context: context,
+      //                       builder: (BuildContext context) {
+      //                         return AlertDialog(
+      //                           title: const Text('Delete note'),
+      //                           content: const Text(
+      //                               'Are you sure you want to delete this note?'),
+      //                           actions: <Widget>[
+      //                             TextButton(
+      //                               child: const Text('Cancel'),
+      //                               onPressed: () =>
+      //                                   Navigator.of(context).pop(),
+      //                             ),
+      //                             TextButton(
+      //                               child: const Text('Delete'),
+      //                               onPressed: () {
+      //                                 setState(() {
+      //                                   deleteNote(notes[index]['_id']);
+      //                                   Navigator.of(context).pop();
+      //                                   Navigator.push(
+      //                                     context,
+      //                                     MaterialPageRoute(
+      //                                         builder: (context) =>
+      //                                             const NoteList()),
+      //                                   );
+      //                                 });
+      //                               },
+      //                             ),
+      //                           ],
+      //                         );
+      //                       },
+      //                     );
+      //                   },
+      //                   icon: const Icon(
+      //                     Icons.delete,
+      //                     color: Colors.red,
+      //                   ),
+      //                 ),
+      //               ],
+      //             )),
+      //       ),
+      //     );
+      //   },
+      // ),
       floatingActionButton: FloatingActionButton(
           backgroundColor: const Color.fromARGB(255, 215, 195, 137),
           onPressed: () {
