a7ad9b1407314a911549b4b118918a631ea113c3
Stephan Granzow <s.granzow@planhero.de>
2023-05-28 06:50:24 +0900

    new datenschutz

--- frontend/src/Pages/DatenschutzPage.tsx
+++ frontend/src/Pages/DatenschutzPage.tsx
@@ -1,32 +1,24 @@
 const DatenschutzPage = () => {
     return ( 
         <div style={{ backgroundColor: 'white', color: 'black' }}>
-            <h2>Datenschutzerklärung</h2>
-
-            <h3>§ 1 Information über die Erhebung personenbezogener Daten</h3>
-            <p>(1) Im Folgenden informieren wir über die Erhebung personenbezogener Daten bei Nutzung unserer Website. Personenbezogene Daten sind alle Daten, die persönlich auf Sie beziehbar sind, z. B. Name, Adresse, E-Mail-Adressen, Nutzerverhalten.</p>
-            <p>(2) Verantwortlicher gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (DS-GVO) ist [Ihr Name, Ihre Adresse, Ihre E-Mail-Adresse, ggf. Ihre Telefonnummer].</p>
-
-            <h3>§ 2 Ihre Rechte</h3>
-            <p>(1) Sie haben gegenüber uns folgende Rechte hinsichtlich der Sie betreffenden personenbezogenen Daten:</p>
-            <ul>
-                <li>Recht auf Auskunft,</li>
-                <li>Recht auf Berichtigung oder Löschung,</li>
-                <li>Recht auf Einschränkung der Verarbeitung,</li>
-                <li>Recht auf Widerspruch gegen die Verarbeitung,</li>
-                <li>Recht auf Datenübertragbarkeit.</li>
-            </ul>
-            <p>(2) Sie haben zudem das Recht, sich bei einer Datenschutz-Aufsichtsbehörde über die Verarbeitung Ihrer personenbezogenen Daten durch uns zu beschweren.</p>
-
-            <h3>§ 3 Erhebung personenbezogener Daten bei Besuch unserer Website</h3>
-            <p>(1) Bei der bloß informatorischen Nutzung der Website, also wenn Sie sich nicht registrieren oder uns anderweitig Informationen übermitteln, erheben wir nur die personenbezogenen Daten, die Ihr Browser an unseren Server übermittelt.</p>
-            <p>(2) Bei Ihrer Kontaktaufnahme mit uns per E-Mail oder über ein Kontaktformular wird Ihre E-Mail-Adresse und, falls Sie dies angeben, Ihr Name und Ihre Telefonnummer von uns gespeichert, um Ihre Fragen zu beantworten.</p>
-
-            <h3>§ 4 Speicherung von Login-Daten</h3>
-            <p>(1) Bei der Anmeldung für unseren Service, speichern wir die von Ihnen eingegebenen Login-Daten ausschließlich zum Zweck der Identifizierung und der Nutzung unseres Dienstes.</p>
-            <p>(2) Die Speicherung und Verarbeitung dieser Daten erfolgt auf Servern, die sich in Deutschland befinden, unter Beachtung aller relevanten Datenschutzgesetze und -bestimmungen.</p>
-            <p>(3) Ihre Login-Daten werden nicht an Dritte weitergegeben oder für andere Zwecke als die hier genannten verwendet.</p>
-            <p>(4) Die Verarbeitung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. b DS-GVO, soweit diese Daten zur Vertragserfüllung oder zur Durchführung vorvertraglicher Maßnahmen erforderlich sind.</p>
+            <h1>Datenschutz&shy;erkl&auml;rung</h1>
+<h2>1. Datenschutz auf einen Blick</h2>
+<h3>Allgemeine Hinweise</h3> 
+<p>Die folgenden Hinweise geben einen einfachen &Uuml;berblick dar&uuml;ber, was mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie pers&ouml;nlich identifiziert werden k&ouml;nnen. Ausf&uuml;hrliche Informationen zum Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgef&uuml;hrten Datenschutzerkl&auml;rung.</p>
+<h3>Datenerfassung auf dieser Website</h3> 
+<h4>Wer ist verantwortlich f&uuml;r die Datenerfassung auf dieser Website?</h4> 
+<p>Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen Kontaktdaten k&ouml;nnen Sie dem Abschnitt &bdquo;Hinweis zur Verantwortlichen Stelle&ldquo; in dieser Datenschutzerkl&auml;rung entnehmen.</p> 
+<h4>Wie erfassen wir Ihre Daten?</h4>
+ <p>Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z.&nbsp;B. um Daten handeln, die Sie in ein Kontaktformular eingeben.</p>
+  <p>Andere Daten werden automatisch oder nach Ihrer Einwilligung beim Besuch der Website durch unsere IT-Systeme erfasst. Das sind vor allem technische Daten (z.&nbsp;B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese Website betreten.</p> 
+  <h4>Wof&uuml;r nutzen wir Ihre Daten?</h4> <p>Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gew&auml;hrleisten. Andere Daten k&ouml;nnen zur Analyse Ihres Nutzerverhaltens verwendet werden.</p> 
+  <h4>Welche Rechte haben Sie bez&uuml;glich Ihrer Daten?</h4> <p>Sie haben jederzeit das Recht, unentgeltlich Auskunft &uuml;ber Herkunft, Empf&auml;nger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben au&szlig;erdem ein Recht, die Berichtigung oder L&ouml;schung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur Datenverarbeitung erteilt haben, k&ouml;nnen Sie diese Einwilligung jederzeit f&uuml;r die Zukunft widerrufen. Au&szlig;erdem haben Sie das Recht, unter bestimmten Umst&auml;nden die Einschr&auml;nkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht Ihnen ein Beschwerderecht bei der zust&auml;ndigen Aufsichtsbeh&ouml;rde zu.</p> 
+  <p>Hierzu sowie zu weiteren Fragen zum Thema Datenschutz k&ouml;nnen Sie sich jederzeit an uns wenden.</p>
+<h2>2. Allgemeine Hinweise und Pflicht&shy;informationen</h2>
+<h3>Datenschutz</h3>
+ <p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer pers&ouml;nlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend den gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerkl&auml;rung.</p>
+ <p>Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie pers&ouml;nlich identifiziert werden k&ouml;nnen. Die vorliegende Datenschutzerkl&auml;rung erl&auml;utert, welche Daten wir erheben und wof&uuml;r wir sie nutzen. Sie erl&auml;utert auch, wie und zu welchem Zweck das geschieht.</p> 
+ <p>Wir weisen darauf hin, dass die Daten&uuml;bertragung im Internet (z.&nbsp;B. bei der Kommunikation per E-Mail) Sicherheitsl&uuml;cken aufweisen kann. Ein l&uuml;ckenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht m&ouml;glich.</p>
         </div>
      );
 }
